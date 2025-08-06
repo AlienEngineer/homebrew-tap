9394c101847542085382ec848f12f278dabc867f
Sergio Ferreira <sergio.ferreira@ctw.bmwgroup.com>
2025-08-06 16:04:17 +0100

    fix username

--- Formula/is-my-code-great.rb
+++ Formula/is-my-code-great.rb
@@ -1,7 +1,7 @@
 class IsMyCodeGreat < Formula
   desc "CLI to analyse Dart test code quality"
-  homepage "https://github.com/AlienEngineer/is-my-code-great"
-  url      "https://github.com/AlienEngineer/is-my-code-great/archive/v0.2.0.tar.gz"
+  homepage "https://github.com/alienengineer/is-my-code-great"
+  url      "https://github.com/alienengineer/is-my-code-great/archive/v0.2.0.tar.gz"
   sha256   "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   def install
