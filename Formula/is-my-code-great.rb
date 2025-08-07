f4a2224b9d810bd7d11c6ac1619bb2ead1f41e49
Sérgio Ferreira <Aimirim.Software@gmail.com>
2025-08-07 11:24:28 +0100

    Update is-my-code-great.rb

--- Formula/is-my-code-great.rb
+++ Formula/is-my-code-great.rb
@@ -1,7 +1,7 @@
 class IsMyCodeGreat < Formula
   desc "CLI to analyse Dart test code quality"
   homepage "https://github.com/alienengineer/is-my-code-great"
-  url      "https://github.com/alienengineer/is-my-code-great/archive/v0.4.1.tar.gz"
+  url      "https://github.com/alienengineer/is-my-code-great/archive/v0.4.2.tar.gz"
   sha256   "c5c279cd9d9307b336fb9ece379829e1bb11f39037c81615477dfeb9b26e9966"
 
   def install
