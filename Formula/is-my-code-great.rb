40001015c91c2e1d3c351d6ded3027190e16b4a8
AlienEngineer <AlienEngineer@gmail.com>
2025-08-19 17:55:39 +0000

    Automated formula update to v0.10.15

--- Formula/is-my-code-great.rb
+++ Formula/is-my-code-great.rb
@@ -1,8 +1,8 @@
 class IsMyCodeGreat < Formula
   desc "CLI to analyse Dart test code quality"
   homepage "https://github.com/alienengineer/is-my-code-great"
-  url      "https://github.com/alienengineer/is-my-code-great/archive/v0.10.14.tar.gz"
-  sha256   "18ad9ffc8e6ad1ceac81e0a5742b3ef8b74f9af35c0601f92f019aec148909c8"
+  url      "https://github.com/alienengineer/is-my-code-great/archive/v0.10.15.tar.gz"
+  sha256   "49ab7a8687379f04c3c2932a14d9d0fbd973f85916c7523cd553e544e706c5dd"
 
   def install
     lib.install Dir["lib/*"]
